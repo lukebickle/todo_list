17a03e70f84577fdafc74978f31752121b846e6e
luke bickle <lukebickle@yahoo.com>
2015-02-22 22:38:59 -0600

    mark as complete capacity

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -8,7 +9,8 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
+
+
     if @todo_item.destroy
       flash[:success] = "Todo List item was deleted."
     else
@@ -17,7 +19,11 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
 
+  end
 
 
 
@@ -30,10 +36,12 @@ class TodoItemsController < ApplicationController
       @todo_list = TodoList.find(params[:todo_list_id])
     end
 
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
+
     def todo_item_params
       params[:todo_item].permit(:content)
     end
-
-
 
 end
